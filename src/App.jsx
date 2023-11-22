1a4805fc10dceb213587c500a83905bcee0396fd
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-11-22 22:55:52 +0300

    fontawesome ve google fonts eklendi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import Header from "./layout/Header";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div>
+      <Header />
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    </div>
   );
 }
