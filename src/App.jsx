e057bd164f26a92193de06c7b61df1ff3f6f76b8
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2024-01-04 01:59:58 +0300

    added route about contact team page

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,33 @@ import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 import Home from "./page/Home";
 import ProductList from "./page/ProductList";
+import Contact from "./page/Contact";
+import About from "./page/About";
+import Team from "./page/Team";
+import Product from "./page/Product";
 
 export default function App() {
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden font-['Montserrat']">
       <Header />
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/productlist">
+        <Route exact path="/shopping">
           <ProductList />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/team">
+          <Team />
+        </Route>
+        <Route path="/shopping/:productId">
+          <Product />
         </Route>
       </Switch>
       <Footer />
