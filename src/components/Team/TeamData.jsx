f7a502f8b12f928bdbe4c90a7b960aeef4fbb2ff
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2024-01-04 05:11:33 +0300

    Team data moved teamData

---
+++ src/components/Team/TeamData.jsx
@@ -0,0 +1,51 @@
+import teamMember1 from "../../../public/assets/team/teamdata/team-1-user-1.jpg";
+import teamMember2 from "../../../public/assets/team/teamdata/team-1-user-2.jpg";
+import teamMember3 from "../../../public/assets/team/teamdata/team-1-user-3.jpg";
+
+export const TeamData = [
+  {
+    img: teamMember1,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember2,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember3,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember1,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember2,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember3,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember1,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember2,
+    header: "Username",
+    role: "Professsion",
+  },
+  {
+    img: teamMember3,
+    header: "Username",
+    role: "Professsion",
+  },
+];
